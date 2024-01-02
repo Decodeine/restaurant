adbf7b4e2cc862778fdf01431e35be5fad09c37f
Decodeine <abionaadesola@yahoo.com>
2024-01-02 14:34:42 +0100

    static file

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,16 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Quick-start development settings - unsuitable for production
